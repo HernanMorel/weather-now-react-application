0a36a9ff25ad93ee72a9e1ad17c3ba9d2f161b71
HernanMorel <84929479+HernanMorel@users.noreply.github.com>
2022-05-09 10:34:20 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css"
-import { useState } from 'react';
+import {useEffect, useState } from 'react';
 import './App.css';
 {/*the bootstrap library must be imported as always*/}
 
@@ -12,18 +12,16 @@ this would be stored in either an env file then through git ignore through secre
 
   {/*first implementation of useState, which will allow you to track changes in memory, facilitating the syntax and making this app possible through react*/}
   const [inputCity, setInputCity] = useState('')
+  {/*this is the useState hook being implemented, it will intake the city name and allow it to change when needed*/}
   const [data, setData] = useState({})
 
   {/*Here you are editing the api url, so that it becomes dynamic, making cityName a variable which can be changed according to the call*/}
   const getWeatherDetails = (cityName) => {
     if (!cityName) return
     const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey
-
     {/*this is using axios to handle the respond events from the api call. */}
     axios.get(apiURL).then((res) => {
-
       console.log("response", res.data)
-
       setData(res.data)
       {/*here axios is handling the error call, in case there is one*/}
     }).catch((err) => {
@@ -31,7 +29,8 @@ this would be stored in either an env file then through git ignore through secre
     })
   }
   const handleKeypress = e => {
-    //it triggers by pressing the enter key
+    {/*this triggers by pressing the enter key, this wasnt the case initially,
+    I had to bind the enter key to this event in order for it to work, making it easier for the user to access the weather of their desired city.*/}
     if (e.key === 'Enter') {
       handleSearch();
 
@@ -41,11 +40,16 @@ this would be stored in either an env file then through git ignore through secre
   const handleChangeInput = (e) => {
     setInputCity(e.target.value)
   }
+  {/*here the useState hook is being used,setInputCity is now bound to it, making it dynamic, therefore making it useful!*/}
   const handleSearch = () => {
     getWeatherDetails(inputCity)
   }
-
-
+  
+  
+  {/* useEffect(()=>{
+   getWeatherDeatils('San Lorenzo')
+  }), [])*/}
+  {/*that was originally used to test the change in state before the city input was made dynamically*/}
 
   return (
     <div className='col-md-12'>
@@ -55,13 +59,17 @@ this would be stored in either an env file then through git ignore through secre
         <div className='d-grid gap-3 col-4 mt-4'>
           <input type='text' className='form-control'
             value={inputCity}
+            {/*the value is now the inputCity variable destructured*/}
             onKeyPress={handleKeypress}
             onChange={handleChangeInput} />
+              {/*here the onChange property binds this input to a change event when needed.*/}
           <button className='btn btn-primary' type='button'
             onClick={handleSearch}
           >Buscar</button>
         </div>
       </div>
+      {/*this Object.keys(data) method returns an array of a given object's own enumerable property names, iterated in the same order that a normal loop would.
+      this is what returns and displays the weather information onto the dom*/}
       {Object.keys(data).length > 0 &&
         <div className='col-md-12 text-center mt-5'>
           {/*standard bootstrap stuffs*/}
@@ -70,6 +78,7 @@ this would be stored in either an env file then through git ignore through secre
             <img className='weatherIcon'
               src='https://i.pinimg.com/originals/77/0b/80/770b805d5c99c7931366c2e84e88f251.png' alt="weather-logo" />
             <h5 className='weatherCity'>{data?.name}</h5>
+            {/*turns out the data that comes from the weather api is in Kelvin units, you need to convert it to celsius*/}
             <h6 className='weatherTemp'>{((data?.main?.temp) - 273.15).toFixed(2)}°C
             </h6>
           </div>
